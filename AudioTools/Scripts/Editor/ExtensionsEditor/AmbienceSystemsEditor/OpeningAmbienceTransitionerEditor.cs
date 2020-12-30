6a373344530764caed5c5888631f0db7f5a8308b
Ville Ojala <69209034+VilleOjala@users.noreply.github.com>
2020-12-30 14:56:19 +0200

    Add files via upload

---
+++ AudioTools/Scripts/Editor/ExtensionsEditor/AmbienceSystemsEditor/OpeningAmbienceTransitionerEditor.cs
@@ -0,0 +1,31 @@
+﻿// Audio Implementation Tools for FMOD and Unity
+// Copyright 2020, Ville Ojala, All rights reserved.
+
+using UnityEngine;
+using UnityEditor;
+
+namespace AudioTools
+{
+    [CustomEditor(typeof(OpeningAmbienceTransitioner))]
+    public class OpeningAmbienceTransitionerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var targetScript = target as OpeningAmbienceTransitioner;
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Add Opening Spot Ambience"))
+            {
+                var spotGameObj = new GameObject();
+                spotGameObj.name = "OpeningSpotAmbience";
+                spotGameObj.transform.parent = targetScript.transform;
+
+                var spotAmbienceComponent = spotGameObj.AddComponent<OpeningSpotAmbience>();
+                targetScript.openingSpotAmbiences.Add(spotAmbienceComponent);
+            }
+        }
+    }
+}
